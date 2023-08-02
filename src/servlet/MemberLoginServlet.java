98316e5ab2e8b69e020ac85a670cafb702e66a8c
dimanranawaka <dimanranawaka@gmail.com>
2023-08-03 00:23:50 +0530

    Implement - fix the bug

--- src/servlet/MemberLoginServlet.java
+++ src/servlet/MemberLoginServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
+import dao.LoginDao;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/memberLogin")
 
 public class MemberLoginServlet extends HttpServlet {
 
@@ -20,5 +26,24 @@ public class MemberLoginServlet extends HttpServlet {
 
         String mUserName = req.getParameter("mUserName");
         String mPassword = req.getParameter("mPassword");
+
+        if (LoginDao.validateLogin(mUserName,mPassword)){
+
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("MemberEvent.html");
+
+            requestDispatcher.forward(req,resp);
+
+        }else {
+
+            writer.println("<center><h1>!! Username or Password Invalid !!</h1></center>");
+
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("MemberLogin.html");
+
+            requestDispatcher.include(req,resp);
+
+        }
+
+        writer.close();
+
     }
 }
