8149e792f6dcd3aa9fbca4dbf828696dadfb9033
dimanranawaka <dimanranawaka@gmail.com>
2023-08-03 00:02:53 +0530

    Implement - a LoginDao

---
+++ src/dao/LoginDao.java
@@ -0,0 +1,47 @@
+package dao;
+
+import db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDao {
+
+    public static boolean validateLogin(String mUserName,String mPassword){
+
+        boolean status = false;
+
+        try {
+
+            Connection connection = DBConnection.getInstance().getConnection();
+
+            // The Question mark(?) Specifies the line number of members table
+
+            String sql = "SELECT * FROM members WHERE mUserName=? AND mPassword=?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1,mUserName);
+            preparedStatement.setString(2,mPassword);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            status = resultSet.next(); // Go to next Position
+
+            connection.close();
+
+
+
+        } catch (SQLException | ClassNotFoundException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return status;
+
+    }
+
+}
--- src/servlet/MemberLoginServlet.java
+++ src/servlet/MemberLoginServlet.java
@@ -1,20 +1,11 @@
 package servlet;
 
-import db.DBConnection;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@WebServlet(urlPatterns = "/memberLogin")
 
 public class MemberLoginServlet extends HttpServlet {
 
@@ -23,47 +14,11 @@ public class MemberLoginServlet extends HttpServlet {
 
         resp.setContentType("text/html");
 
-        String userName = req.getParameter("mUserName");
-        String userPassword = req.getParameter("mPassword");
-
         PrintWriter writer = resp.getWriter();
 
-        try {
+        // Fetching user input username and password
 
-            DBConnection instance = DBConnection.getInstance();
-
-            String sql = "SELECT mUserName,mPassword From members";
-
-            PreparedStatement preparedStatement = instance.getConnection().prepareStatement(sql);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            String uS = resultSet.getString(1);
-            String uP = resultSet.getString(2);
-
-            if (userName.equals(uS) && userPassword.equals(uP)){
-
-                RequestDispatcher requestDispatcher = req.getRequestDispatcher("MemberEvent.html");
-                requestDispatcher.forward(req,resp);
-
-                preparedStatement.close();
-                instance.getConnection().close();
-
-
-            }else {
-
-                writer.println("<center><h1>!! Username or Password Invalid !!</h1></center>");
-
-                RequestDispatcher requestDispatcher = req.getRequestDispatcher("MemberLogin.html");
-
-                requestDispatcher.include(req,resp);
-            }
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        }
-        writer.close();
+        String mUserName = req.getParameter("mUserName");
+        String mPassword = req.getParameter("mPassword");
     }
 }
