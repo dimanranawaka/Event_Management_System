d60e67787e793a3a92bd4735db5dc9508f1fccb5
dimanranawaka <dimanranawaka@gmail.com>
2023-08-03 08:38:57 +0530

    Implement - EventRegistrarServlet

--- src/servlet/EventRegistrarServlet.java
+++ src/servlet/EventRegistrarServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
+import db.DBConnection;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/eventRegistrar")
 
@@ -14,7 +20,50 @@ public class EventRegistrarServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        super.doPost(req, resp);
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
 
+        // Fetching User Inputs
+
+        String eName = req.getParameter("eName");
+        String eNum = req.getParameter("eNum");
+        String cardNum = req.getParameter("cardNum");
+        String eDate = req.getParameter("eDate");
+        String cardCVV = req.getParameter("cardCVV");
+        String cHoldersName = req.getParameter("cHoldersName");
+
+        try {
+
+            DBConnection instance = DBConnection.getInstance();
+
+            String sql = "INSERT INTO paymentdetails(eName,eNum,cardNum,eDate,cardCVV,cHoldersName) VALUES (?,?,?,?,?,?)";
+
+            PreparedStatement preparedStatement = instance.getConnection().prepareStatement(sql);
+
+            preparedStatement.setString(1,eName);
+            preparedStatement.setString(2,eNum);
+            preparedStatement.setString(3,cardNum);
+            preparedStatement.setString(4,eDate);
+            preparedStatement.setString(5,cardCVV);
+            preparedStatement.setString(6,cHoldersName);
+
+            preparedStatement.executeUpdate();
+
+            writer.println("<h1>!! Payment Successfully !!</h1>");
+
+            System.out.println("Payment Added to Database!");
+
+            preparedStatement.close();
+            instance.getConnection().close();
+
+            RequestDispatcher reqRequestDispatcher = req.getRequestDispatcher("EventRegistrar.html");
+            reqRequestDispatcher.include(req,resp);
+
+        } catch (SQLException | ClassNotFoundException e) {
+
+            e.printStackTrace();
+
+        }
+        writer.close();
     }
 }
