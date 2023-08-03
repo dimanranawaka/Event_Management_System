ac936d90c821f4aa6170b94cfc907944e884489d
dimanranawaka <dimanranawaka@gmail.com>
2023-08-03 07:19:22 +0530

    Config - EventRegistrar Page

---
+++ src/servlet/EventRegistrarServlet.java
@@ -0,0 +1,20 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/eventRegistrar")
+
+public class EventRegistrarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        super.doPost(req, resp);
+
+    }
+}
